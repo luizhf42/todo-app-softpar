425258fb6d23ca8b9e055b79d264e6a9e2cf9227
luizhf42 <luizhf42@gmail.com>
2025-01-04 14:44:42 -0300

    feat: set up layout and route

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>Lista de Tarefas</h1>
+  </header>
+
+  <main><router-view /></main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="css" scoped>
+header {
+  background-color: var(--primary);
+  color: white;
+  text-align: center;
+  padding: 0.5rem;
+
+  h1 {
+    height: min-content;
+    margin: 0;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+}
+
+main {
+  width: min(90%, 600px);
+  margin: 0 auto;
+  padding: 1rem 0;
+}
+</style>
